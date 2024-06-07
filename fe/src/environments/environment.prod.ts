c8f93e153021aa7b5810832fe3f927432486b45f
Loki <lokeshreddy.art@gmail.com>
2024-06-07 14:23:04 +0530

    changed frontend env file to change production restapi uri

--- fe/src/environments/environment.prod.ts
+++ fe/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   test: true,
-  host: "https://uat-api.hustlehub.tech",
-  socketUrl: "https://uat-api.hustlehub.tech",
+  host: "https://api.hustlehub.tech",
+  socketUrl: "https://api.hustlehub.tech",
   port: '',
   version: "v1",
   deskTypes: ["HotDesks", "FixedDesks", "PrivateOffice", "ServicedOffice"],
